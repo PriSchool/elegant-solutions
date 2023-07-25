60b56e5ca8ee4ea94263729ad3fa66d457624a6e
Tomer Priel <tomerprielg@gmail.com>
2023-07-25 14:54:36 +0300

    Update spiralMatrix.py

--- spiralMatrix.py
+++ spiralMatrix.py
@@ -1,3 +1,5 @@
+# our goal is to take any given M x N matrix, and return a list of it's values traversed in a spiral fashion
+
 matrix = [
     [1,2,3],
     [4,5,6],
@@ -6,11 +8,13 @@ matrix = [
     [13,14,15]
 ]
 
+# solution is going to be -> [1, 2, 3, 6, 9, 12, 15, 14, 13, 10, 7, 4, 5, 8, 11]
+
 solution = []
 
 while matrix:
     solution.extend(matrix.pop(0))
-    matrix = [*zip(*matrix)][::-1]
+    matrix = [*(zip(*matrix))][::-1]
 print(solution)
 
 # matrix.pop(0)
@@ -22,15 +26,13 @@ print(solution)
 # *matrix
 # [4,5,6] [7,8,9] [10,11,12] [13,14,15] - post splat operation
 
-# *zip(*matrix)
+# *(zip(*matrix))
 # (4,7,10,13) (5,8,11,14) (6,9,12,15) - post zip operation with another splat, turns them into individual tuples
 
-# [*zip(*matrix)]
+# [*(zip(*matrix))]
 # [(4,7,10,13), (5,8,11,14), (6,9,12,15)] - using list syntax [] around the product turns them into indexed list items
 
-# [*zip(*matrix)][::-1]
+# [*(zip(*matrix))][::-1]
 # [(6,9,12,15), (5,8,11,14), (4,7,10,13)] - [::-1] reverses their order as list items, not each item's order
 
-# this process causes the next loop to essentially pop the "top row" of the matrix again, until we're left with no more values to work with
-
-# solution = [1, 2, 3, 6, 9, 12, 15, 14, 13, 10, 7, 4, 5, 8, 11]
+# this process causes the next loop to essentially pop the "top row" of the matrix again, until we're left with no more values to work with, and returning the list
